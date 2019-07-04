c7cf792499ea56587329adf55e3b97b6e7aa4ed9
xiaohuanghao <pp1118005@gmail.com>
2019-07-04 17:28:09 +0800

    第三次提交

--- l03_component/src/main/java/com/demo/l03_component/MainActivity.java
+++ l03_component/src/main/java/com/demo/l03_component/MainActivity.java
@@ -2,13 +2,43 @@ package com.demo.l03_component;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.SimpleTimeZone;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_main_test1).setOnClickListener(this);
+        findViewById(R.id.btn_main_test2).setOnClickListener(this);
+        findViewById(R.id.btn_main_test3).setOnClickListener(this);
+        findViewById(R.id.btn_main_test4).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_main_test1:
+                startActivity(new Intent(this,SimpleComponentActivity.class));
+                break;
+
+                case R.id.btn_main_test2:
+                break;
+
+                case R.id.btn_main_test3:
+                break;
+
+                case R.id.btn_main_test4:
+                break;
+
+                default:
+                    break;
+        }
+
     }
 }
---
+++ l03_component/src/main/java/com/demo/l03_component/SimpleComponentActivity.java
@@ -0,0 +1,14 @@
+package com.demo.l03_component;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SimpleComponentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_component);
+    }
+}
