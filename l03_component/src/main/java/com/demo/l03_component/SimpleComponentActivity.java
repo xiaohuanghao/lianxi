168841ba03ae987dbb0d36e145caf5086ac90d9a
xiaohuanghao <pp1118005@gmail.com>
2019-07-05 17:23:39 +0800

    1

--- l03_component/src/main/java/com/demo/l03_component/SimpleComponentActivity.java
+++ l03_component/src/main/java/com/demo/l03_component/SimpleComponentActivity.java
@@ -3,12 +3,33 @@ package com.demo.l03_component;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SimpleComponentActivity extends AppCompatActivity {
+
+    private TextView tv_simple_message;
+    private EditText et_simple_number;
+    private Button btn_simple_submit;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_component);
+
+
+        //TextView
+        tv_simple_message =(TextView)findViewById(R.id.tv_simple_message);
+        tv_simple_message.setText("textviwe");
+        //EditText
+        et_simple_number=(EditText)findViewById(R.id.et_simple_number);
+
     }
+
+
+
 }
